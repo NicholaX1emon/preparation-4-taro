8365fe260216ea8015f9a02d49263d98f65a32b7
NicholaX1emon <279633927@qq.com>
2019-06-18 00:51:58 +0800

    detail_almost_done

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,11 +1,102 @@
 // pages/detail/detail.js
+// 初始化数据库
+const db = wx.cloud.database()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    movieDetail: {}
+    movieDetail: {},
+    uploadedImgs: [],
+    comment: '',
+    rating: 5,
+    movieId: -1,
+    fileIds: []
+  },
+
+  uploadImg: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths);
+        this.setData({
+          uploadedImgs: this.data.uploadedImgs.concat(tempFilePaths)
+        });
+      }
+    })
+  },
+
+  submit: function () {
+    wx.showLoading({
+      title: '评论中',
+    })
+    console.log(this.data.comment, this.data.rating);
+
+    // 上传图片到云存储
+    let promiseArr = [];
+    for (let i = 0; i < this.data.uploadedImgs.length; i++) {
+      promiseArr.push(new Promise((reslove, reject) => {
+        let item = this.data.uploadedImgs[i];
+        console.log('uploadingImg --->',item)
+        let suffix = /\.\w+$/.exec(item)[0]; // 正则表达式，返回文件扩展名
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+          filePath: item, // 小程序临时文件路径
+          success: res => {
+            // 返回文件 ID
+            console.log(res.fileID)
+            this.setData({
+              fileIds: this.data.fileIds.concat(res.fileID)
+            });
+            reslove();
+          },
+          fail: console.error
+        })
+      }));
+    }
+
+    Promise.all(promiseArr).then(res => {
+      // 插入数据
+      db.collection('comment').add({
+        data: {
+          comment: this.data.comment,
+          rating: this.data.rating,
+          movieid: this.data.movieId,
+          fileIds: this.data.fileIds
+        }
+      }).then(res => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '评价成功',
+        })
+      }).catch(err => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '评价失败',
+        })
+      })
+    })
+  },
+
+  onCommentChange: function (event) {
+    console.log('e --->', event)
+    this.setData({
+      comment: event.detail
+    });
+  },
+
+  onRatingChange: function (event) {
+    console.log('e --->', event)
+    this.setData({
+      rating: event.detail
+    });
   },
 
   /**
